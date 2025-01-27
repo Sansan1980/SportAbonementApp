4dc216091cd5c70683849069775e92b00407ccb8
Homework-10 <danilovsansan@gmail.com>
2025-01-27 20:28:15 +0300

    Initial commit

---
+++ src/main/java/com/danilov/sport_abonement_app/SportAbonementAppApplication.java
@@ -0,0 +1,13 @@
+package com.danilov.sport_abonement_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportAbonementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportAbonementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilov/sport_abonement_app/SportAbonementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilov.sport_abonement_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportAbonementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
