c342c55bc85b99741781164a5a31350d880e9f43
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2025-02-01 19:08:53 +0300

    Dobavil interpheis customer

--- src/main/java/com/danilov/sport_abonement_app/service/customerservice/CustomerService.java
+++ src/main/java/com/danilov/sport_abonement_app/service/customerservice/CustomerService.java
@@ -5,10 +5,10 @@ import com.danilov.sport_abonement_app.model.Employee;
 public interface CustomerService {
     String addCustomer(String name, String surname, int numberTelephone );
 
-    Employee findCustomer(String name, String surname, int numberTelephone);
-    String updateCustomer(String name, String surname, int numberTelephone );//здесь возможно ошибка
+  /*  String updateCustomer(String name, String surname, int numberTelephone );//здесь возможно ошибка
 
     String deleteCustomer(String name, String surname, int numberTelephone);
+    Employee findCustomer(String name, String surname, int numberTelephone);*/
 
 
 }
--- src/main/java/com/danilov/sport_abonement_app/service/customerservice/CustomerServiceImpl.java
+++ src/main/java/com/danilov/sport_abonement_app/service/customerservice/CustomerServiceImpl.java
@@ -1,16 +1,38 @@
 package com.danilov.sport_abonement_app.service.customerservice;
 
+import com.danilov.sport_abonement_app.model.Customer;
 import com.danilov.sport_abonement_app.model.Employee;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class CustomerServiceImpl implements CustomerService {
-    String addCustomer(String name, String surname, int numberTelephone );
+    private static Map<String, Customer> customerMap = new HashMap<>();
 
-    Employee findCustomer(String name, String surname, int numberTelephone);
-    String updateCustomer(String name, String surname, int numberTelephone );//здесь возможно ошибка
+    String addCustomer(String name, String surname, int numberTelephone) {
+        //добавиить сюда валидацию string и Integer
+        if (customerMap.size() >= 1000) {
+            System.out.println("превышен лемит клиентов");
+        } else if (customerMap.containsKey(key(name, surname))) {
+            System.out.println("Такой сотрудник уже существует");
+        } else {
+            customerMap.put(key(name, surname), new Customer(name, surname, numberTelephone));
+        }
+        return "Введены новые данные,  " + name + " " + surname; // + предстваить номер телефона через стринг;//решить тоже самое через стримы
+    }
 
-    String deleteCustomer(String name, String surname, int numberTelephone);
+       /* Employee findCustomer (String name, String surname,int numberTelephone){
+        }
 
+        String updateCustomer (String name, String surname,int numberTelephone){
+        }//здесь возможно ошибка
 
+        String deleteCustomer (String name, String surname,int numberTelephone){
+        }*/
+
+    private String key(String name, String surname) {
+        return name + surname;
+    }
 }
